3074c5ec4e045a410737bf5d5d61f3502d9ade43
Diana <beshaorobinsonstein1991@okakmail.com>
2026-06-25 17:21:09 +0300

    Disable demo data loading for Railway production environment

--- src/main/java/com/ganttpro/GanttproApplication.java
+++ src/main/java/com/ganttpro/GanttproApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
@@ -18,6 +19,7 @@ public class GanttproApplication {
     }
 
     @Bean
+    @Profile("!railway")
     public CommandLineRunner loadDemoData(UserRepository userRepository,
                                          ProjectRepository projectRepository,
                                          TaskRepository taskRepository,
